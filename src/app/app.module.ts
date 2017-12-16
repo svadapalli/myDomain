1d4b2bf1576e86ab897b18067a13ed30240fb258
svadapalli <satya.aquarianadm@gmail.com>
2017-12-16 17:00:25 -0500

    Intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { WorkComponent } from './components/work/work.component';
+
+export const ROUTES: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'work', component: WorkComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    WorkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
